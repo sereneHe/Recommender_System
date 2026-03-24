d72cae26d6ca8cee370723a660c5288120ac6db8
Pavel Rytir <rytirpavel@icloud.com>
2026-03-24 11:20:30 +0100

    cluster script cds

---
+++ scripts/experiments_recommender_cds.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+export PYTHONPATH=$PYTHONPATH:../
+
+CMD="python3 run_experiments.py --multirun --config-name=config-cluster"
+
+
+${CMD} experiment="CDS_RECOMMENDER" solver="mark_with_cc, mark" problem=cds problem.target="'05ABBF', '8B69AP', '06DABK', 'EFAGG9', '2H6677', 'FH49GG', '8D8575', 'GG6EBT', 'DD359M', 'FF667M', '0H99B7', '2H66B7', '8A87AG', 'NN2A8G', '6A516F'"
+
